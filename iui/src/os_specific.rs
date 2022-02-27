af1864a1209bc921b60c8feb34239407823600c1
Jomy10 <jonas.vbs4@gmail.com>
2022-02-27 19:44:12 +0100

    Import resolved

--- iui/src/os_specific.rs
+++ iui/src/os_specific.rs
@@ -1,14 +1,16 @@
 //! OS Specific functions
 
-use crate::controls::Window;
-use crate::UI;
-
-
 #[cfg(target_os = "macos")]
-use objc::{sel, sel_impl};
-
-#[cfg(target_os = "macos")]
-pub unsafe fn center_window(ui: &UI, window: &mut Window) {
-    let handle = ui_sys::uiControlHandle(window.ptr() as *mut ui_sys::uiControl) as *mut objc::runtime::Object;
-    let _: () = objc::msg_send![handle, center];
+mod macos {
+    use crate::controls::Window;
+    use crate::UI;
+    
+    extern crate objc;
+    use self::objc::{sel, sel_impl, msg_send};
+    
+    pub unsafe fn center_window(ui: &UI, window: &mut Window) {
+        let handle = ui_sys::uiControlHandle(window.ptr() as *mut ui_sys::uiControl) as *mut objc::runtime::Object;
+        let _: () = msg_send![handle, center];
+    }
 }
+
