7f660aa63fe14657170d2a06a3662a3981e66313
Jonas Everaert <62475953+Jomy10@users.noreply.github.com>
2022-02-27 19:05:57 +0100

    OS-specific `center_window` for MacOS

--- iui/src/os_specific.rs
+++ iui/src/os_specific.rs
@@ -4,7 +4,7 @@
 use objc::{sel, sel_impl};
 
 #[cfg(target_os = "macos")]
-unsafe fn center_window(ui: &UI, window: &mut Window) {
+pub unsafe fn center_window(ui: &UI, window: &mut Window) {
     let handle = ui_sys::uiControlHandle(window.ptr() as *mut ui_sys::uiControl) as *mut objc::runtime::Object;
     let _: () = objc::msg_send![handle, center];
 }
